89264fe77e6220522ea2ffdb79c0af9ae62a3d2e
Robert Wittmann <Robert Wittmann>
2016-03-29 12:13:53 +0200

    Added shit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 package src;
 
 public class Main {
-    
+    public static void main(String[] args){
+    	Scanner sc = new Scanner(System.in);
+
+    	System.out.print("Hello Sir/Madam, what may I call you? ");
+    	String i = sc.next();
+		System.out.println("Greetings, " + i + " !");
+		sc.close();
+    }
 }
